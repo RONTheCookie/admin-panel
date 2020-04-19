ccd3af8e987c3f38181f7805aed171a7e92ee2c6
Ron B <me@ronthecookie.me>
2020-04-19 23:27:59 +0300

    add sdab logs

--- app.js
+++ app.js
@@ -3,20 +3,28 @@ const app = express();
 const child = require("mz/child_process");
 const port = process.env.PORT || 3000;
 require("log-node")();
-const log = require("log"); 
+const log = require("log");
 const dbOpts = {
     db: process.env.db || `sdab`,
 };
-if (process.env.DB_ADDR) dbOpts.servers = [{ host: process.env.DB_ADDR.split(":")[0], port: parseInt(process.env.DB_ADDR.split(":")[1], 10) }]
+if (process.env.DB_ADDR)
+    dbOpts.servers = [
+        {
+            host: process.env.DB_ADDR.split(":")[0],
+            port: parseInt(process.env.DB_ADDR.split(":")[1], 10),
+        },
+    ];
 const r = require(`rethinkdbdash`)(dbOpts); // Connect to RethinkDB
 app.use(express.static("assets"));
 app.set("view engine", "ejs");
 
-async function getWatchtowerLogs() {
+async function getContainerLogs(containerName) {
     try {
-        return (await child.exec("docker logs new_watchtower_1 --tail 10")).map(a => a.toString()).join("");
+        return (await child.exec(`docker logs ${containerName} --tail 10`))
+            .map((a) => a.toString())
+            .join("");
     } catch (err) {
-        return "ERROR: couldn't get watchtower logs."
+        return "ERROR: couldn't get watchtower logs.";
     }
 }
 async function getBuilds() {
@@ -25,8 +33,9 @@ async function getBuilds() {
 
 app.get("/", async (req, res) => {
     const builds = await getBuilds();
-    const watchtowerLogs = await getWatchtowerLogs();
-    res.render("index", { builds, watchtowerLogs });
+    const watchtowerLogs = await getContainerLogs("new_watchtower_1");
+    const sdabLogs = await getContainerLogs("new_sdab_1");
+    res.render("index", { builds, watchtowerLogs, sdabLogs });
 });
 
 app.listen(port, () => log(`Listening on port ${port}`));
